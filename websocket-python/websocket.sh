62fc5246527daa90e06e806fc17e6e9e9ec98664
Derista ID <95257653+dendikusnandiii@users.noreply.github.com>
2022-01-24 07:54:47 +0700

    Add files via upload

---
+++ websocket-python/websocket.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+clear
+echo Menginstal Websocket-SSH Python
+sleep 1
+echo Cek Hak Akses...
+sleep 0.5
+cd
+#Install system auto run
+#System OpenSSH Websocket-SSH Python
+cd
+cd /etc/systemd/system/
+wget -O /etc/systemd/system/ws-openssh.service https://raw.githubusercontent.com/dendikusnandiii/deristaws/main/websocket-python/ws-openssh.service
+#System Dropbear Websocket-SSH Python
+wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/dendikusnandiii/deristaws/main/websocket-python/ws-dropbear.service
+#System SSL/TLS Websocket-SSH Python
+wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/dendikusnandiii/deristaws/main/websocket-python/ws-stunnel.service
+##System Websocket-OpenVPN Python
+wget -O /etc/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/dendikusnandiii/deristaws/main/websocket-python/ws-ovpn.service
+
+#Install Script Websocket-SSH Python
+cd
+cd /usr/local/bin/
+wget -O /usr/local/bin/ws-openssh https://raw.githubusercontent.com/dendikusnandiii/deristaws/main/websocket-python/ws-openssh && chmod +x /home/admin/ws-openssh
+wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/dendikusnandiii/deristaws/main/websocket-python/ws-dropbear && chmod +x /home/admin/bin/ws-dropbear
+wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/dendikusnandiii/deristaws/main/websocket-python/ws-stunnel && chmod +x /home/admin/ws-stunnel
+wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/dendikusnandiii/deristaws/main/websocket-python/ws-ovpn && chmod +x /home/admin/ws-ovpn
+#
+chmod +x /usr/local/bin/ws-openssh
+
+cd
+#
+cd
+systemctl daemon-reload
+#Enable & Start & Restart ws-openssh service
+systemctl enable ws-openssh.service
+systemctl start ws-openssh.service
+systemctl restart ws-openssh.service
+
+#Enable & Start & Restart ws-dropbear service
+systemctl enable ws-dropbear.service
+systemctl start ws-dropbear.service
+systemctl restart ws-dropbear.service
+
+#Enable & Start & Restart ws-stunnel service
+systemctl enable ws-stunnel.service
+systemctl start ws-stunnel.service
+systemctl restart ws-stunnel.service
+
+
+#Enable & Start ws-ovpn service
+systemctl enable ws-ovpn.service
+systemctl start ws-ovpn.service
+systemctl restart ws-ovpn.service
